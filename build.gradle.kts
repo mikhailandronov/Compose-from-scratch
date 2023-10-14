993d96429a6249c1434dbcb10a6327fd3a03bc43
andronov <mikhail_andronov@mail.ru>
2023-10-14 19:30:51 +0300

    Init Compose project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.compose") version "1.5.0"
+}
+
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+}
